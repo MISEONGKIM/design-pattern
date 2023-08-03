30253f7702fd9cf11de5ff8b15c69685ea387624
MISEONGKIM <rlaaltjd46@jinhakapply.com>
2023-08-03 17:19:50 +0900

    feat : Adapter 패턴추가

---
+++ 호환/Adapter/src/PrintBanner.ts
@@ -0,0 +1,15 @@
+import { Banner } from "./Banner";
+import { Print } from "./Print";
+//합성의 방법
+export class PrintBanner implements Print {
+  private banner: Banner;
+  constructor(str: string) {
+    this.banner = new Banner(str);
+  }
+  printWeak(): void {
+    this.banner.showWithParen();
+  }
+  printStrong(): void {
+    this.banner.showWithAster();
+  }
+}
